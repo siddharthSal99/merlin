fd50a6d6613c891d66bb7fbe1d11ec26fdf38f2e
Max Rudolph <maxrudolph@lawn-128-61-21-41.lawn.gatech.edu>
2019-10-26 23:02:26 -0400

    whatever

--- cv/find_chess_pose.py
+++ cv/find_chess_pose.py
@@ -1,12 +1,12 @@
 import cv2
 import numpy as np
 
-def find_chess_corners(file_name, num_corners):
-    c = cv2.imread(file_name)
-    found, corners = cv2.findChessboardCorners(c, (7,9))
-    img = cv2.drawChessboardCorners(c, (7,9), corners, found)
+def find_chess_corners(mat, num_corners):
+    
+    found, corners = cv2.findChessboardCorners(mat, num_corners)
+    img = cv2.drawChessboardCorners(mat, (7,9), corners, found)
     cv2.imshow('drawn', img)
-    cv2.imwrite('output.png', img)
+    
 
 
 
